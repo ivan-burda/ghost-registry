0e64e644e5b6155059b3b3d8e6444c9aad16c471
Nik Sumeiko <nik.sumeiko@gmail.com>
2026-05-30 17:16:37 +0200

    chore: Add storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,25 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default tseslint.config(
-    { ignores: ['dist'] },
-    {
-        extends: [js.configs.recommended, ...tseslint.configs.recommended],
-        files: ['src/**/*.{ts,tsx}'],
-        plugins: {
-            'react-hooks': reactHooks,
-            'react-refresh': reactRefresh,
-        },
-        rules: {
-            ...reactHooks.configs.recommended.rules,
-            'react-hooks/purity': 'off',
-            'no-useless-assignment': 'off',
-            'react-refresh/only-export-components': [
-                'warn',
-                { allowConstantExport: true },
-            ],
-        },
+export default tseslint.config({ ignores: ['dist'] }, {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['src/**/*.{ts,tsx}'],
+    plugins: {
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
     },
-);
+    rules: {
+        ...reactHooks.configs.recommended.rules,
+        'react-hooks/purity': 'off',
+        'no-useless-assignment': 'off',
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+    },
+}, storybook.configs["flat/recommended"]);
