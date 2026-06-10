6f58029cfe51da5e4b4533fc0bc19b1162ed3dba
ivan-burda <burda.ivan@protonmail.com>
2026-06-10 18:17:16 +0200

    create useGhostEditForm hook

---
+++ src/EditGhost/useGhostEditForm.ts
@@ -0,0 +1,11 @@
+import {editGhostPageSchema, GhostFormData} from "./schema.ts";
+import {useForm, UseFormReturn} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+
+export const useGhostEditForm = (defaultValues: GhostFormData): UseFormReturn<GhostFormData> => {
+    return useForm<GhostFormData>({
+        resolver: zodResolver(editGhostPageSchema),
+        defaultValues,
+    });
+}
